f009b3ea83af59ffa75cd35a170c989da92c4a48
Rahul Nanda <146613927+rahulnanda11@users.noreply.github.com>
2025-05-29 20:27:24 +0530

    Time: 137 ms (55.11%) | Memory: 59.7 MB (56.98%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -7,9 +7,7 @@ class Solution:
             for i in range(start,n+1):
                 path.append(i)
                 backtrack(i+1,path)
-                path.pop()
+                path.pop()     
         result=[]
         backtrack(1,[])
         return result
-
-        
